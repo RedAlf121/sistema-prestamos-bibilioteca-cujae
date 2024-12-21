9364015dc178f6683744bd72206af0d560b7a33a
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2024-12-20 21:21:37 -0500

    dark and light mode for email's html template

--- whatsapptest.py
+++ whatsapptest.py
@@ -6,7 +6,7 @@ from messager.messager import send
 #pywhatkit.sendwhatmsg(phone_no='+5356707158',message='pareces un singao comepinga hablando solo mi tanke',time_hour=23, time_min=54)
 
 if __name__ == "__main__":
-      customer = Customer('Alejandro','aleessu@gmail.com',[],Category.STUDENT)
+      customer = Customer('Osvaldo','osvaldo020213@gmail.com',[],Category.STUDENT)
       send('email',customer=customer)
       print('Hecho')
 
